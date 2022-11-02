243bdb48150d655955c9ed8e5b2ae0ec8685f85e
Eugenia Anello <61031596+eugeniaring@users.noreply.github.com>
2022-11-02 17:40:36 +0100

    Add files via upload

--- ecg_app.py
+++ ecg_app.py
@@ -1,55 +1,38 @@
 import pandas as pd
 import numpy as np
+import json
+import boto3
 import os
 
 import streamlit as st
 import requests
 import zipfile
 
-#from servicerequest import make_request_to_bento_service
 from src.create_data import read_dataset
 from src.visualize import visualize_predict_vs_gt
+from request import query_endpoint
 
 if not os.path.exists('test_eval'):
     os.mkdir('test_eval')
 
-SERVICE_URL = "https://ke496mufza.execute-api.us-west-1.amazonaws.com/predict"
+global app_name
+global region
+app_name = 'ifTest'
+region = "us-east-1"
+
 model_name = 'if'
 
 st.markdown('# **Web App to detect Anomalies from ECG signals**')
-#bar = st.progress(0)
 
 file_csv = st.sidebar.file_uploader("Choose CSV file to evaluate model",type=["csv"])
 
 button = st.sidebar.button('Check Anomalies!')
 
-
 def filter_df(df,patient_id):
     subset_df = df[df.patient_id==patient_id]
-    return subset_df,subset_df.label.values
-
-def transform_string(s):
-    s = s.replace('[','').replace(']','').replace(',','')
-    l = s.split(" ")
-    return np.array(list(map(int, l)))
-
-def map_label(y):
-    y[y == 1] = 0
-    y[y == -1] = 1
-    return y   
-
-def make_request_to_bento_service(
-    service_url: str, df: pd.DataFrame
-) -> np.array:
-    serialized_input_data = df.to_json()
-    response = requests.post(
-        service_url,
-        data=serialized_input_data,
-        headers={"content-type": "application/json"}
-    )
-    y = transform_string(response.text)
-    y = map_label(y)
-    return y
+    label = subset_df.label.values
+    subset_df = subset_df[['heart_rate','hr_diff','peak_label']]
+    return subset_df,label
 
 def save_zip(l_patient_ids):
     list_files = ["test_eval/gt_vs_pred_{}_if.html".format(patient_id) for patient_id in l_patient_ids]
@@ -63,11 +46,12 @@ if file_csv is not None and button is not None:
     l_ids = list(patients_df.patient_id.unique())
     rows = []
     for idx in l_ids:
-        patient_df,expected_output = filter_df(patients_df,idx)
-        pred = make_request_to_bento_service(SERVICE_URL, patient_df)
+        input_data,expected_output =  filter_df(patients_df,idx)
+        input_data = input_data.to_json(orient="split")
+        predictions = query_endpoint(app_name=app_name, input_json=input_data)
         # print(f"prediction: {pred}")
         # print(f"Expected output: {expected_output.astype(int)}")
-        rows.extend(pred)
+        rows.extend(predictions)
     patients_df['test_preds']=rows
 
     patient_id = st.selectbox('Patient id',tuple(l_ids))
---
+++ request.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import json
+import boto3
+
+from src.create_data import read_dataset
+
+global app_name
+global region
+app_name = 'ifTest'
+region = "us-east-1"
+
+def sample_random_ecg_data_patient():
+    test_df = read_dataset('ecg_data/test.csv','test')
+    test_df = test_df[test_df.patient_id==14]
+    expected_output = test_df.label.values
+    test_df = test_df[['heart_rate','hr_diff','peak_label']]
+    return test_df,expected_output
+
+
+
+def query_endpoint(app_name, input_json):
+    """ Invoke the SageMaker endpoint and send the 
+    input request to be processed 
+    """
+    client = boto3.session.Session().client('sagemaker-runtime', region)
+
+    response = client.invoke_endpoint(
+        EndpointName = app_name,
+        Body = input_json,
+        ContentType = 'application/json; format=pandas-split',
+        )
+
+    preds = response['Body'].read().decode('ascii')
+    preds = np.array(json.loads(preds))
+    preds[preds == 1] = 0
+    preds[preds == -1] = 1 
+    print('Received response: {}'.format(preds))
+    return preds
+
+
+# Convert input into json for SageMaker endpoint
+input_data,expected_output = sample_random_ecg_data_patient()
+input_data = input_data.to_json(orient="split")
+
+print('Expected_output: ',expected_output)
+# compare ground truth with predictions
+predictions = query_endpoint(app_name=app_name, input_json=input_data)
